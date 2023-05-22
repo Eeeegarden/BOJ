11c3c1eab8f06a994594d3d1f70b8b3da7cfecc2
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2023-05-23 00:41:59 +0900

    Create BOJ_5397.cpp

---
+++ BOJ_5397.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int case_num;
+    cin >> case_num;
+
+    for(int i=0; i<case_num; i++){
+        string S;
+        cin >> S;
+        list<char> L={};
+        auto cursor = L.begin();
+
+        for(auto c : S){
+            if(c == '<'){
+                if(cursor != L.begin())cursor--;
+            }
+            else if(c == '>'){
+                if(cursor != L.end())cursor++;
+            }
+            else if(c == '-'){
+                if(cursor != L.begin()){
+                    cursor--;
+                    L.erase(cursor);
+                }
+            }
+            else {
+                L.insert(cursor,c);
+            }
+        }
+        
+        for(auto c : L){
+            cout << c;
+        }
+        cout << "\n" ;
+    }
+}
+                
+
+

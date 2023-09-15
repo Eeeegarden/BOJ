ee845c365914c004c2e1a078485f4aadfce237cb
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2023-09-15 23:48:15 +0900

    backtracking

---
+++ BOJ_15652.cpp
@@ -0,0 +1,29 @@
+//30초컷
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[8];
+
+void func(int k){
+    if(k==m){
+        for(int i=0; i<m; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=1; i<=n; i++){
+        if(arr[k-1]>i)continue;
+        arr[k]=i;
+        func(k+1);
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    func(0);
+}

ac0875108db8d8a2b3fd286bd534bb864f64defc
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-01-23 23:52:16 +0900

    Backtracking

--- [0x0C]Backtracking/BOJ_1941.cpp
+++ [0x0C]Backtracking/BOJ_1941.cpp
@@ -21,41 +21,57 @@ int main(){
         for(int j=0; j<5; j++){
             queue<pair<int,int>> Q;
             bool vis[6][6]={0,};
+            int curvis=0;
+            char curchar;
             int cnt=0;
             int ynum=0;
             Q.push({i,j});
             vis[i][j]=1;
+            if(s[i][j]=='Y')ynum++;
+            cnt++;
             while(!Q.empty()){
                 auto cur = Q.front(); Q.pop();
+                if(vis[cur.X][cur.Y]!=0 && curvis>vis[cur.X][cur.Y]){
+                    if(curchar=='Y')ynum--;
+                    cnt--;
+                }
                 for(int dir=0; dir<4; dir++){
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
                     if(nx<0 || ny<0 || nx>=5 || ny>=5)continue;
-                    if(vis[nx][ny]==1)continue;
+                    if(vis[nx][ny]!=0)continue;
                     if(ynum==3 && s[nx][ny]=='Y')continue;
                     Q.push({nx,ny});
-                    vis[nx][ny]=1;
+                    vis[nx][ny]=vis[cur.X][cur.Y]+1;
+                    curvis=vis[nx][ny];
+                    curchar=s[nx][ny];
                     cnt++;
                     if(s[nx][ny]=='Y')ynum++;
                 }
                 if(cnt==7 && ynum<=3){
                     ans++;
+                    cout << "----------------" << '\n';
+                    cout << i << j <<'\n';
+                    for(int q=0; q<5; q++){
+                        for(int w=0; w<5; w++){
+                            cout << vis[q][w];
+                        }
+                        cout << '\n';
+                    }
+                    cout << "----------------" << '\n';
                     break;
                 }
                 cout << "----------------" << '\n';
-                cout << i << j <<'\n';
-                for(int q=0; q<5; q++){
-                    for(int w=0; w<5; w++){
-                        cout << vis[q][w];
+                    cout << i << j <<' ' <<cnt <<'\n';
+                    for(int q=0; q<5; q++){
+                        for(int w=0; w<5; w++){
+                            cout << vis[q][w];
+                        }
+                        cout << '\n';
                     }
-                    cout << '\n';
-                }
-                cout << "----------------" << '\n';
+                    cout << "----------------" << '\n';
             }
         }
     }
     cout << ans;
 }
-
-
-

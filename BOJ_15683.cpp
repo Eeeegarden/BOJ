e4b147ed10692e283dd620a5e3ebf61f959e229d
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-02-06 18:57:31 +0900

    Simulation

--- BOJ_15683.cpp
+++ BOJ_15683.cpp
@@ -1,80 +1,99 @@
-// 푸는중
-// 하루 이틀 걸려도 무조건 구현해보기
-// cctv 최대개수 8개 이하 , 각 cctv 탐색 최대 4가지경우의수
-// --> 백트래킹쓰면 최대 4^8 -> 약65000개 밖에안됨 ㄱㄱ
-// 5번 cctv 아니면 배열에 넣어서 next_permutation 으로 순서 정해서 다 돌려보기 ?
+// BKD 풀이방법 참조..
+
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
 #define Y second
 
-int N,M;
 int m[10][10];
-int vis[10][10];
-vector<pair<int,int>> arr;
-int dx[4]={1,-1,0,0};
+int m2[10][10];
+int N,M;
+int mn=100;
+int dx[4]={-1,1,0,0};
 int dy[4]={0,0,-1,1};
+vector<pair<int,int>> cctv;
 
-//상하좌우 다돌림
-void cctv5(int i, int j){
-    queue<pair<int,int>> Q;
-    for(int dir=0; dir<4; dir++){
-        int cnt=0;
-        Q.push({i,j});
-        vis[i][j]=1;
-        while(!Q.empty()){
-            auto cur = Q.front(); Q.pop();
-            int nx = cur.X + dx[dir];
-            int ny = cur.Y + dy[dir];
-            if(nx<0 || ny<0 || nx>=N || ny>=M)continue;
-            if(m[nx][ny]==6)continue;
-            if(vis[nx][ny]!=0)continue;
-            vis[nx][ny]=1;
-            if(cnt<=vis[nx][ny])cnt=vis[nx][ny];
-            Q.push({nx,ny});
-        }
+// cctv 감시 범위
+void upd(int x,int y,int dir){
+    dir=dir%4;
+    while(1){
+        x = x + dx[dir];
+        y = y + dy[dir];
+        if(x<0 || y<0 || x>=N || y>=M)return;
+        if(m[x][y]==6)return;
+        if(m[x][y]!=0)continue;
+        m2[x][y]=7;
     }
 }
 
-void cctv2(int i, int j){
-    int c_vis[10][10]
-}
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> N >> M;
+
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin >> m[i][j];
+            m2[i][j]=m[i][j];
+            if(m[i][j]!=0 && m[i][j]!=6)cctv.push_back({i,j});
         }
     }
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            if(m[i][j]==5)cctv5(i,j);
-            if(m[i][j]!=0 && m[i][j]<=4)arr.push_back({i,j});
+    // cctv가 4개의 방향을 가지고 있을때의 방향의 경우의수 -> i는 4^cctv.size() 까지 참조 -> 4진수 풀이
+    for(int i=0; i<(1<<(2*cctv.size())); i++){
+        int temp=i;
+        int cnt=0;
+        for(int j=0; j<cctv.size(); j++){
+            int dir = temp%4;
+            int x=cctv[j].X;
+            int y=cctv[j].Y;
+            // dx,dy 무빙이 상하좌우 순이라 if문 처리 어려움
+            // 시계방향 무빙이였으면 조금 더 간결한 풀이가능
+            if(m[x][y]==1) upd(x,y,dir);
+            else if(m[x][y]==2){
+                if(dir==0 || dir==2){
+                    upd(x,y,dir);
+                    upd(x,y,dir+1);
+                }
+                else {
+                    upd(x,y,dir);
+                    upd(x,y,dir+3);
+                }
+            }
+            else if(m[x][y]==3){
+                if(dir<2){
+                    upd(x,y,0);
+                    upd(x,y,dir+2);
+                }
+                else if(dir<4){
+                    upd(x,y,1);
+                    upd(x,y,dir);
+                }
+            }
+            else if(m[x][y]==4){
+                upd(x,y,dir);
+                upd(x,y,dir+1);
+                upd(x,y,dir+2);
+            }
+            else if(m[x][y]==5){
+                upd(x,y,dir);
+                upd(x,y,dir+1);
+                upd(x,y,dir+2);
+                upd(x,y,dir+3);
+            }
+            temp/=4;
         }
+
+        for(int q=0; q<N; q++){
+            for(int w=0; w<M; w++){
+                if(m2[q][w]==0)cnt++;
+            }
+        }
+
+        if(cnt<=mn)mn=cnt;
+        copy(m[0],m[0]+100,m2[0]);
     }
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            cout << vis[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-
+    cout << mn;
 }
-
-
-    // for(int dir=0; dir<4; dir++){
-    //     vector<bool> v(4-dir,false);
-    //     v.insert(v.end(),dir,true);
-    //     cout << "----" << dir << "개----\n";
-    //     do{
-    //         for(int k=0; k<4; k++){
-    //             if(v[k])cout << k ;
-    //         }
-    //         cout << '\n';
-    //     }while(next_permutation(v.begin(),v.end()));
-    // }

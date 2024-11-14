41316d26a6d6ee8f9817411867c456b3017a6aa3
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2024-11-15 00:40:20 +0900

    달팽이 숫자

--- [0xff]Swea/d2/1954.cpp
+++ [0xff]Swea/d2/1954.cpp
@@ -1,3 +1,7 @@
+// DFS 라 생각해서 스택인줄 알았지만 스택으로 안풀림 -> Queue활용
+// 변형 DFS?
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define X first
@@ -11,7 +15,7 @@ int Round=0;
 int dx[4]={0,1,0,-1};
 int dy[4]={1,0,-1,0};
 int dir;
-stack<pair<int,int>> Q;
+queue<pair<int,int>> Q;
 
 int main(){
     ios::sync_with_stdio(0);
@@ -27,7 +31,6 @@ int main(){
         int max_N = number[i];
         Round++;
         cout << "#" << Round << '\n';
-        // 맵 초기화
         for(int j=0; j<11; j++){
             for(int k=0; k<11; k++){
                 Map[j][k]=0;
@@ -40,21 +43,27 @@ int main(){
         vis[0][0]=1;
         dir=0;
 
-        // while(!Q.empty()){
-        //     auto cur = Q.top(); Q.pop();
-        //     int nx = cur.X + dx[dir];
-        //     int ny = cur.Y + dy[dir];
-        //     if(nx<0 || ny<0 || nx>=max_N || ny>=max_N || vis[nx][ny]!=0 || Map[nx][ny]!=0){
-        //         dir=(dir+1)%4;
-        //         nx = cur.X + dx[dir];
-        //         ny = cur.Y + dy[dir];
-        //     }
-            
-        //     Map[nx][ny]=Map[cur.X][cur.Y]+1;
-        //     vis[nx][ny]=1;
-        //     Q.push({nx,ny});
-            
-        // }
+        while(!Q.empty()){
+            auto cur = Q.front();
+
+            // 4방향 탐색 
+            for(int i=0; i<4; i++){
+                int nx = cur.X + dx[dir];
+                int ny = cur.Y + dy[dir];
+                // 못가면 90도 회전
+                if(nx<0 || ny<0 || nx>=max_N || ny>=max_N || vis[nx][ny]!=0 || Map[nx][ny]!=0){
+                    dir=(dir+1)%4;
+                    continue;
+                }
+                // 갈 수 있으면 증가
+                Map[nx][ny]=Map[cur.X][cur.Y]+1;
+                vis[nx][ny]=1;
+                Q.push({nx,ny});
+                break;
+            }
+            // Queue에 1개만 유지
+            if(!Q.empty())Q.pop();
+        }
 
         for(int i=0; i<max_N; i++){
             for(int j=0; j<max_N; j++){

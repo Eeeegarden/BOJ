bedfadebcda50385333ea39563df699911741dbc
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2025-02-20 21:31:15 +0900

    N과 M(4) - S3

---
+++ [0xff]RandomDefence/BOJ_15652.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M;
+int arr[10];
+
+void func(int k){
+    if(k==M){
+        for(int j=0; j<k; j++){
+            cout << arr[j] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=1; i<=N; i++){
+        if(k!=0 && i<arr[k-1])continue;
+        arr[k]=i;
+        func(k+1);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    func(0);
+}
